635888638fa8cf9a74ec9e69f0b410d75f678a2b
mswindler <matthew@ionixdesign.com>
2017-10-13 15:25:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ angular.module('News', [])
     '$scope',
     function($scope){
       $scope.test = 'Hello world!';
+        $scope.posts = [
+            'Post 1',
+            'Post 2',
+            'Post 3',
+    		'Post 4',
+    		'Post 5'
+        ];
     }
     ]);
+	
