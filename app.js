a9e856fa30d6c79df8625e5bf7fd12be5b18282a
mswindler <matthew@ionixdesign.com>
2017-10-16 15:30:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ angular.module('News', ['ui.router'])
           templateUrl: '/home.html',
           controller: 'MainCtrl'
         });
+	.state('posts', {
+   		url: '/posts/{id}',
+   		templateUrl: '/posts.html',
+    		controller: 'PostCtrl'
+  	});    
       $urlRouterProvider.otherwise('home');
   }])
 
@@ -24,21 +29,35 @@ angular.module('News', ['ui.router'])
     'postFactory',
     function($scope, postFactory){
       $scope.posts = postFactory.posts;
-//     {title:'Post 1', upvotes:5},
-//     {title:'Post 2', upvotes:6},
-//     {title:'Post 3', upvotes:1},
-//     {title:'Post 4', upvotes:4},
-//     {title:'Post 5', upvotes:3}
-//         ];
 	    
 	$scope.addPost = function() {
     	 $scope.posts.push({title:$scope.formContent,upvotes:0});
    	 $scope.formContent='';
   	};
 	    
-	    	$scope.incrementUpvotes = function(post) {
+	 $scope.incrementUpvotes = function(post) {
     		post.upvotes += 1;
-  		};
+  	 };
     }
-    ]);
+    ])
+
+  .controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'postFactory', 
+    function($scope, $stateParams, postFactory){
+      $scope.post = postFactory.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
+    $scope.incrementUpvotes = function(comment){
+      comment.upvotes += 1; 
+    };
+  }]);
+
 	
