0cc51ba1ce3e65a4da0aeb73034347c87f15e56d
mswindler <matthew@ionixdesign.com>
2017-10-16 15:16:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-angular.module('News', [])
-    .controller('MainCtrl', [
+angular.module('News', ['ui.router'])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+      $urlRouterProvider.otherwise('home');
+  }])
+
+  .factory('postFactory', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+
+ .controller('MainCtrl', [
     '$scope',
-    function($scope){
-      $scope.test = 'Hello world!';
-        $scope.posts = [
-    {title:'Post 1', upvotes:5},
-    {title:'Post 2', upvotes:6},
-    {title:'Post 3', upvotes:1},
-    {title:'Post 4', upvotes:4},
-    {title:'Post 5', upvotes:3}
-        ];
+    'postFactory',
+    function($scope, postFactory){
+      $scope.posts = postFactory.posts;
+//     {title:'Post 1', upvotes:5},
+//     {title:'Post 2', upvotes:6},
+//     {title:'Post 3', upvotes:1},
+//     {title:'Post 4', upvotes:4},
+//     {title:'Post 5', upvotes:3}
+//         ];
 	    
 	$scope.addPost = function() {
     	 $scope.posts.push({title:$scope.formContent,upvotes:0});
