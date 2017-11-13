a8397bc1837e494d3f14780eaeb6b24a9c1b5e01
mswindler <matthew@ionixdesign.com>
2017-11-13 15:47:49 -0700

    Update app.js

--- FullMeanStack/full/app.js
+++ FullMeanStack/full/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
-require('./models/comments');
+require('./models/Comments');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
