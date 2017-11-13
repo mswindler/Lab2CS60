b81e1b044477c8d988469a1e6bb290c8d72fb171
mswindler <matthew@ionixdesign.com>
2017-11-13 22:48:46 +0000

    test

--- FullMeanStack/full/app.js
+++ FullMeanStack/full/app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
+require('./models/comments');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- FullMeanStack/full/routes/index.js
+++ FullMeanStack/full/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = mongoose.model('comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET comments */
+router.get('/comments', function(req, res, next) {
+  Comment.find(function(err, comments){
+    if(err){ return next(err); }
+    res.json(comments);
+  });
+});
+
+/* POST comments */
+router.post('/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+    res.json(comment);
+  });
 });
 
 module.exports = router;
